b7152ebb1620c633e2a009db48da879c4371a20f
thenurisenethma <thenurisenethma@gmail.com>
2025-07-24 21:19:21 +0530

    first commit

---
+++ src/main/java/com/uflipage/uflipage/UFliPageApplication.java
@@ -0,0 +1,13 @@
+package com.uflipage.uflipage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UFliPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UFliPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uflipage/uflipage/UFliPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uflipage.uflipage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UFliPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
